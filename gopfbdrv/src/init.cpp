5d49f4cd283a179690a7602b1faa4ecf2f9f8687
Mike <kdx7214@gmail.com>
2021-03-20 00:28:30 -0500

    Update GOP driver to search/set native mode

--- gopfbdrv/src/init.cpp
+++ gopfbdrv/src/init.cpp
@@ -34,9 +34,11 @@ uint64_t write(void *buffer, uint64_t size)
 
 extern "C" preload_driver *DriverInit(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) 
 {
-	EFI_GUID						gopguid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID ;
-	EFI_GRAPHICS_OUTPUT_PROTOCOL	*gop ;
-	EFI_STATUS						status = EFI_ABORTED ;
+	EFI_GUID								gopguid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID ;
+	EFI_GRAPHICS_OUTPUT_PROTOCOL			*gop ;
+	EFI_GRAPHICS_OUTPUT_MODE_INFORMATION	*info ;
+	EFI_STATUS								status = EFI_ABORTED ;
+	UINTN									size, num, native ;
 
 	pd.write = write ;
 	pd.read = (preload_read)NULL ;
@@ -60,9 +62,41 @@ extern "C" preload_driver *DriverInit(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *
 	pd.data[1] = size ;
 	pd.data[2] = width ;
 	pd.data[3] = height ;
-	pd.error = 420 ;
+	pd.error = 777 ;
 
 
+	// Get the current mode information so we can scan the modes for native resolution
+	status = gop->QueryMode(gop, gop->Mode == NULL ? 0 : gop->Mode->Mode, &size, &info) ;
+	if (status == EFI_NOT_STARTED) 
+		status = gop->SetMode(gop, 0) ;
+
+	if (status != EFI_SUCCESS)				// On error, just return current video mode information
+		return &pd ;
+
+	if (native != gop->Mode->Mode)			// Already in native resolution
+		return &pd ;
+
+	native = gop->Mode->Mode ;
+	num = gop->Mode->MaxMode ;
+	status = gop->QueryMode(gop, native, &size, &info) ;
+	if (status != EFI_SUCCESS)
+		return &pd ;
+
+	status = gop->SetMode(gop, native) ;
+	if (status != EFI_SUCCESS)
+		return &pd ;
+
+	fbaddr = (void *)gop->Mode->FrameBufferBase ;
+	size = gop->Mode->FrameBufferSize ;
+	width = gop->Mode->Info->HorizontalResolution ;
+	height = gop->Mode->Info->VerticalResolution ;
+	pixperscanline = gop->Mode->Info->PixelsPerScanLine ;
+
+	pd.data[0] =  (uint64_t)fbaddr ;
+	pd.data[1] = size ;
+	pd.data[2] = width ;
+	pd.data[3] = height ;
+	pd.error = 123 ;
 
 	return &pd ;
 
